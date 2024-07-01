5cbad15a955443c9f7e7f76df1083be40de67659
YourmixJNR <radmakson@gmail.com>
2024-07-01 02:27:38 +0100

    added full-width to card component

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card = ({ icon, title, description }: CardProps) => {
   return (
-    <div className="card pr-2 pb-2 isolate">
+    <div className="card w-full pr-2 pb-2 isolate">
       <div className="space-y-6 py-11 px-7 rounded-lg  border-l-[1px] border-t-[1px] border-[#303030] bg-white">
         {icon}
         <h3 className="text-2xl font-bold">{title}</h3>
