55a65900d67a4142344b419665369bb810762047
YourmixJNR <radmakson@gmail.com>
2024-07-08 20:09:20 +0100

    pages title modified

--- src/app/(how-it-works)/components/steps.tsx
+++ src/app/(how-it-works)/components/steps.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Steps = () => {
   return (
-    <div className=" font-lexend px-8 xl:px-24">
+    <div className="font-lexend px-8 xl:px-24">
       {/* <!-- Item #1 --> */}
       <div className="relative pl-8 sm:pl-32 py-12 group">
         <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-full before:px-px before:bg-slate-300 sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-black after:border-4 after:box-content after:border-slate-50 after:rounded-lg sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
--- src/app/(how-it-works)/how-it-works/page.tsx
+++ src/app/(how-it-works)/how-it-works/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Metadata } from "next";
 import DiscoverSwiftclipz from "../components/discover-swiftclipz";
 import Steps from "../components/steps";
 import GetStarted from "@/app/components/get-started";
+
+export const metadata: Metadata = {
+    title: "Discover How Swiftclipz Works"
+}
 
 const Page = () => {
   return (
